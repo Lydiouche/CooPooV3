9dfa26796f12dbb2bde51d6b7b8c0fb22e586bb7
madjen-web <madjenebaadramedrame@gmail.com>
2025-12-03 09:59:35 +0100

    Update Arme.java

--- src/Arme.java
+++ src/Arme.java
@@ -1,2 +1,13 @@
-public class Arme {
+public abstract class Arme extends Entite {
+
+    protected Coordonnees positionA; 
+
+
+    public String getNomA() {
+        return this.getNom();
+    }
+
+    public Coordonnees getPositionA() {
+        return this.positionA;
+    }
 }
