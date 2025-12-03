8b932494175c0b5b6e2094995cfbd89c33999cf6
Alexis <alexiss.boulbes@gmail.com>
2025-12-03 09:50:53 +0100

    pull

---
+++ src/Entiter.java
@@ -0,0 +1,14 @@
+public abstract class Entiter{
+private int X ;
+private int y ;
+private String name ;
+public int getX() {
+    return X;
+}
+public int getY() {
+    return y;
+}
+public String getName() {
+    return name;
+}
+}
