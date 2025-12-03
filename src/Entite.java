49c4b0e4734924281d5c946aa59358e5c9071b92
cthury <cleothury@gmail.com>
2025-12-03 15:56:01 +0100

    Changement constructeur Entite

--- src/Entite.java
+++ src/Entite.java
@@ -3,10 +3,6 @@ public abstract class Entite{
     private int y ;
     private String name ;
 
-public Entite(){
-    x = 0;
-    y = 0;
-}
 public Entite(String name){
     this.name = name;
     x = 0;
@@ -15,8 +11,8 @@ public Entite(String name){
 
 public Entite(String name, int x, int y){
     this.name = name;
-    this.x = 0;
-    this.y = 0;
+    this.x = x;
+    this.y = y;
 }
 
 public int getX() {
