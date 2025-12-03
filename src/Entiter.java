68e37962300115e90521361611c0bd01e058aa00
Alexis <alexiss.boulbes@gmail.com>
2025-12-03 10:14:48 +0100

    up

--- src/Entiter.java
+++ src/Entiter.java
@@ -2,10 +2,8 @@ public abstract class Entiter{
 private int X ;
 private int y ;
 private String name ;
-public Entiter(int X, int y, String name){
-    this.X = X;
-    this.y = y;
-    this.name = name;
+public Entiter(){
+    
 }
 public int getX() {
     return X;
