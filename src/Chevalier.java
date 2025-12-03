f44ad8a22375fd1f908cefeac8ceece9de010d4e
Alexis <alexiss.boulbes@gmail.com>
2025-12-03 15:12:37 +0100

    up

--- src/Chevalier.java
+++ src/Chevalier.java
@@ -1,2 +1,9 @@
-public class Chevalier {
+public class Chevalier extends Personne {
+    public Chevalier(String nom) {
+        super(nom);
+    }
+    @Override
+    public void attaquer() {
+        System.out.println("Le Chevalier donne un coup d'épée (Dégâts élevés).");
+    }
 }
